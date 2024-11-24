5a27d912eadd20fcbeb22099be6b5d2c0112ae3e
Andri Yngvason <andri@yngvason.is>
2024-11-24 23:23:53 +0000

    Add an example authentication script

---
+++ scripts/auth-script.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# This is an example script that displays a pop-up dialogue for user
+# authentication.
+# It can be enabled via the --auth-script option. E.g.
+#     wlvncc --auth-command=$HOME/projects/wlvncc/scripts/auth-script.h my-vnc-server.local
+
+ENTRY=$(zenity --password --username)
+
+case $? in
+	0)
+		echo "$ENTRY" | tr '|' "\n"
+		;;
+	1)
+		echo "Stop login." &>2
+		;;
+	-1)
+		echo "An unexpected error has occurred." &>2
+		;;
+esac
